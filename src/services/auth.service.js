b50d408917e466fdea25fd54eaf96177015af1d5
Kris Lai <kris.lai@igsl-group.com>
2021-07-16 17:33:11 +0800

    updated: refresh auth tokens logic

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,8 @@
+const jwt = require('jsonwebtoken');
 const httpStatus = require('http-status');
 const tokenService = require('./token.service');
 const userService = require('./user.service');
+const config = require('../config/config');
 const Token = require('../models/token.model');
 const ApiError = require('../utils/ApiError');
 const { tokenTypes } = require('../config/tokens');
@@ -39,12 +41,11 @@ const logout = async (refreshToken) => {
  */
 const refreshAuth = async (refreshToken) => {
   try {
-    const refreshTokenDoc = await tokenService.verifyToken(refreshToken, tokenTypes.REFRESH);
-    const user = await userService.getUserById(refreshTokenDoc.user);
-    if (!user) {
+    const payload = jwt.verify(refreshToken, config.jwt.secret);
+    const user = payload.trunk || null;
+    if (!user || payload.sub !== user.empNum) {
       throw new Error();
     }
-    await refreshTokenDoc.remove();
     return tokenService.generateAuthTokens(user);
   } catch (error) {
     throw new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate');
